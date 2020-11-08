0c424f874fbafae98981cec211aae9cd2cacad50
alireza darbandi <darbandi1996@gmail.com>
2020-11-09 02:49:49 +0330

    implemented migration

--- install.sh
+++ install.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 [ !  -d "vendor" ] && composer install
 [ !  -d "vendor" ] && chown -R www-data:www-data /var/www/html
-# php artisan migrate:fresh --seed
+sleep 10 # waiting on postgres container to show up
+php artisan migrate --seed
 /usr/sbin/apache2ctl -D FOREGROUND
