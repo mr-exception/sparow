1dfe1a111796ecb9ac5839f8619be3747a4b3bbe
alireza darbandi <darbandi1996@gmail.com>
2020-11-16 00:59:56 +0330

    changed project structure

--- install.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/sh
-[ !  -d "vendor" ] && composer install
-[ !  -d "node_modules" ] && npm i
-[ !  -d "vendor" ] && chown -R www-data:www-data /var/www/html
-sleep 10 # waiting on postgres container to show up
-php artisan migrate --seed
-/usr/sbin/apache2ctl -D FOREGROUND
--- resources/views/welcome.blade.php
+++
@@ -1,3 +0,0 @@
-hello! I'm a sparow
-<div id="example"></div>
-<script type="text/javascript" src="js/app.js"></script>
